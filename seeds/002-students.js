c7c457c53e8deb0a345e7cccb240999f356b2dee
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-03-26 18:50:16 -0400

    add seeds

---
+++ seeds/002-students.js
@@ -0,0 +1,18 @@
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('students')
+    .del()
+    .then(() => knex('cohorts'))
+    .then(function(cohorts) {
+      // generate random entries
+      const entries = [];
+      for (let i = 0; i < 10; i++) {
+        entries.push({
+          name: `student${i}`,
+          cohort_id: cohorts[Math.floor(Math.random() * cohorts.length)].id
+        });
+      }
+      // Inserts seed entries
+      return knex('students').insert(entries);
+    });
+};
